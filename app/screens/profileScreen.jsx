97ff7bcd453cce23ec57d911e9b59de750fc4d54
g-onelli <gabriela.k.onelli@gmail.com>
2023-07-14 23:33:24 -0400

    adding navigation capacity pt1

---
+++ app/screens/profileScreen.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Image, TouchableHighlight } from 'react-native';
+
+function profileScreen(props) {
+    return (
+       <TouchableHighlight onPress={() => }>
+        <Image>
+        </Image>
+       </TouchableHighlight> 
+
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default profileScreen;
